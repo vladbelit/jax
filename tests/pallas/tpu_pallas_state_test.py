3d791b5f02633b5b23a275d12ed63f7a030c39b7
Vladimir Belitskiy <147626444+vladbelit@users.noreply.github.com>
2026-04-27 14:42:31 -0400

    Fix TPU Pallas tests for sorted HLO backend configs

--- tests/pallas/tpu_pallas_state_test.py
+++ tests/pallas/tpu_pallas_state_test.py
@@ -258,7 +258,8 @@ class PinnedBufferTest(jtu.JaxTestCase):
     hlo = f.lower(x).compile().as_text()
     self.assertRegex(
         hlo,
-        r'output_memory_space_colors.*color.*1'
+        r'b\(f32\[3\]\{[^}]*S\(1\)[^}]*\}\) custom-call\([^)]*\), '
+        r'custom_call_target="tpu_custom_call"',
     )
 
 
